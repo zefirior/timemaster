23e043b62db28a210e00c7e1aa8d27db2b731804
Daniil Galiev <dgaliev@rc-online.org>
2017-10-27 10:18:56 +0500

    смена вьюх

--- src/core/mainUI.py
+++ src/core/mainUI.py
@@ -18,24 +18,6 @@ class MainApp(QMainWindow):
         self.workspace = None
         self.views = {}
 
-    def get_worklayout(self):
-        return self.workspace.findChildren(QVBoxLayout, 'verticalLayout_5')[0]
-
-    def clearWorkSpace(self):
-        layout = self.get_worklayout()
-        for child in layout.children():
-            child.hide()
-            # layout.removeWidget(child)
-            # child.setParent(None)
-            # child.deleteLater()
-            # self.workspace.setLayout(layout)
-
-    def setWorkSpace(self, view):
-        layout = self.get_worklayout()
-
-        layout.addWidget(view)
-        self.workspace.setLayout(layout)
-
 
 class WindowSetuper(Ui_MainWindow):
     def __init__(self, main):
@@ -45,21 +27,55 @@ class WindowSetuper(Ui_MainWindow):
         self.menu_button = {}
 
         self.setupUi(self.main)
-        self.main.workspace = self.verticalFrame_2
+        self.workspace = self.verticalFrame_2
         self.menu_build()
+
+    def get_worklayout(self):
+        return self.workspace.findChildren(QVBoxLayout, 'verticalLayout_5')[0]
+
+    def clearWorkSpace(self):
+        layout = self.get_worklayout(); print(layout.count())
+        for childI in range(layout.count()):
+            print('hidden ' + str(childI))
+            layout.itemAt(childI).widget().setHidden(1)
+            # layout.removeWidget(child)
+            # child.setParent(None)
+            # child.deleteLater()
+            # self.workspace.setLayout(layout)
+
+    @staticmethod
+    def wrapView(name):
+        return 'view' + str(name)
+
+    def setWorkSpace(self, view):
+        layout = self.get_worklayout()
+        print(layout)
+        layout.addWidget(view)
+        # self.workspace.setLayout(layout)
+
+    @staticmethod
+    def getWidgetByName(layout, child_name):
+        for chldI in range(layout.count()):
+            w = layout.itemAt(chldI).widget()
+            if w.objectName() == child_name:
+                return w
+            else:
+                print(w.objectName())
 
     def view_exec(self):
         sender = self.main.sender()
         section_name = sender.section_name
-
+        self.clearWorkSpace()
         if section_name in self.views:
             self.views[section_name].rerender()
+            w = self.getWidgetByName(self.get_worklayout(), self.wrapView(section_name))
+            w.setHidden(0)
         else:
             control = controlmap[section_name]
-            self.main.clearWorkSpace()
-            view = control.render(parent=self.main.workspace)
+            view = control.render(parent=self.workspace)
             self.views[section_name] = view
-            self.main.setWorkSpace(view)
+            view.setObjectName(self.wrapView(section_name))
+            self.setWorkSpace(view)
 
     def menu_build(self):
         for section_name in controlmap:
@@ -67,10 +83,12 @@ class WindowSetuper(Ui_MainWindow):
 
             button.setObjectName(section_name)
             self.verticalLayout_4.addWidget(button)
-            self.menu_button[section_name] = button
+            button.setObjectName("pushButton" + section_name)
+            button.setText(section_name)
             button.section_name = section_name
+            self.menu_button[section_name] = button
 
-            button.clicked.connect(self.view_exec())
+            button.clicked.connect(self.view_exec)
 
 
 
