bb53c8da811ee3ee9b81f49a7034dbcbfb3694d4
Daniil Galiev <wizard.liinda@gmail.com>
2018-06-11 18:49:44 +0500

    qthread

--- simple_interval/mainApp.py
+++ simple_interval/mainApp.py
@@ -3,10 +3,10 @@
 import sys
 import logging
 from ui.interval_view import Ui_MainWindow
-from core.task_launcher import TaskLauncher
 # from core.mainUI import app, main, WindowSetuper
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QFrame, QVBoxLayout, QHBoxLayout, QPushButton
-from PyQt5.QtCore import pyqtSlot, QRect
+from PyQt5.QtCore import pyqtSlot, QRect, QThread, pyqtSignal
+from PyQt5 import QtCore
 
 
 class MainApp(QMainWindow):
@@ -21,28 +21,31 @@ class MainApp(QMainWindow):
         self.views = {}
 
 
-class Notificator(QWidget):
-    def __init__(self, main):
-        super().__init__()
-        self.main = main
+class Notificator(object):
+    def __init__(self, app):
+        self.app = app
+        self.main = app
         self.setup()
 
         self.pushButton.clicked.connect(self.on_ok)
         self.pushButton_2.clicked.connect(self.on_cancel)
 
     def setup(self):
-        self.horizontalLayoutWidget = QWidget(self)
-        self.horizontalLayoutWidget.setGeometry(QRect(360, 100, 211, 21))
-        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
-        self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
+        self.General = QWidget()
+        self.General.setGeometry(QRect(360, 100, 211, 21))
+        self.General.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QHBoxLayout(self.General)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.pushButton = QPushButton(self.horizontalLayoutWidget)
+        self.pushButton = QPushButton(self.General)
         self.pushButton.setObjectName("pushButton")
+        self.pushButton.setText("continue")
         self.horizontalLayout.addWidget(self.pushButton)
-        self.pushButton_2 = QPushButton(self.horizontalLayoutWidget)
+        self.pushButton_2 = QPushButton(self.General)
         self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.setText("cancel")
         self.horizontalLayout.addWidget(self.pushButton_2)
+        # QtCore.QMetaObject.connectSlotsByName(self.main)
 
     def on_ok(self):
         logging.info('notificator continue')
@@ -53,19 +56,29 @@ class Notificator(QWidget):
         self.main.schedule_stop()
 
 
-class WindowSetuper(Ui_MainWindow):
-    def __init__(self, main):
-        super(WindowSetuper).__init__()
-        self.main = main
-        self.views = {}
-        self.menu_button = {}
+class DelayThread(QThread):
+    signal = pyqtSignal()
 
-        self.setupUi(self.main)
+    def __init__(self, delay):
+        QThread.__init__(self)
+        self.delay = delay
 
-        self.notificator = None
-        self.tlauncher = TaskLauncher()
+    def __del__(self):
+        self.wait()
+
+    def run(self):
+        self.sleep(self.delay)
+        self.signal.emit()
+
+
+class TimeLauncher(MainApp, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.notificator = Notificator(self)
+
+        self.delay_thread = None
         self.work_state = False
-        self.pushButton.clicked.connect(self.begin_scheduler)
 
         self.schedule = {
             #  id: [delay, name]
@@ -76,7 +89,10 @@ class WindowSetuper(Ui_MainWindow):
         self.schedule_order = [1, 3, 2]
         self.cur_task_id = None
         self.cur_task_name = None
-        # self.run_task(1)
+
+        # connections block
+        self.pushButton.clicked.connect(self.begin_scheduler)
+        # connections block
 
     def run_task(self, id):
         if id not in self.schedule:
@@ -85,15 +101,13 @@ class WindowSetuper(Ui_MainWindow):
         logging.info('run task "{}"'.format(name))
         self.cur_task_id = id
         self.cur_task_name = name
-        self.tlauncher.add_task(delay, self.on_timeout)
+        self.delay_thread = DelayThread(delay)
+        self.delay_thread.signal.connect(self.on_timeout)
+        self.delay_thread.start()
 
     def on_timeout(self):
-        # self.work_state = False
         logging.info('run task "{}"'.format(self.cur_task_name))
-        self.notificator = Notificator(self)
-        self.notificator.show()
-        # self.cur_task_id = None
-        # self.cur_task_name = None
+        self.notificator.General.show()
 
     def begin_scheduler(self):
         if self.work_state:
@@ -104,8 +118,7 @@ class WindowSetuper(Ui_MainWindow):
 
     def schedule_continue(self):
         logging.info('main continue')
-        self.notificator.hide()
-        self.notificator = None
+        self.notificator.General.hide()
         task_id = self.cur_task_id
         index = self.schedule_order.index(task_id)
         if index == len(self.schedule_order) - 1:
@@ -116,8 +129,7 @@ class WindowSetuper(Ui_MainWindow):
 
     def schedule_stop(self):
         logging.info('main stop')
-        self.notificator.hide()
-        self.notificator = None
+        self.notificator.General.hide()
         self.work_state = False
 
 
@@ -127,8 +139,7 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
 
-    main = MainApp()
-    w = WindowSetuper(main)
-    main.show()
+    window = TimeLauncher()
+    window.show()
 
     sys.exit(app.exec_())
