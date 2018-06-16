b7df982133c3ecdbbac4cb14d876606fe06cb2e6
Daniil Galiev <wizard.liinda@gmail.com>
2018-06-16 16:51:11 +0500

    refactor structure

---
+++ deprecated_src/core/__init__.py
@@ -0,0 +1,4 @@
+from .DBContext import DBContext
+from .integrator import Integrator
+from .controlmap import controlmap
+from .BackgroundTimer import TaskLauncher
--- src/core/__init__.py
+++
@@ -1,4 +0,0 @@
-from .DBContext import DBContext
-from .integrator import Integrator
-from .controlmap import controlmap
-from .BackgroundTimer import TaskLauncher
